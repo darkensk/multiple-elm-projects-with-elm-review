99d8eab5898bf29c8604556005c7b66d1a22eb2d
Oliver Polka <polka.oliver@gmail.com>
2022-08-15 11:29:37 +0200

    Yarn workspaces init setup

---
+++ packages/maple/src/Maple/StaticTranslations.elm
@@ -0,0 +1,11 @@
+module Maple.StaticTranslations exposing (heading, headingForOak, headingForBirch)
+
+heading : String
+heading =
+    "This is Maple"
+
+headingForOak =
+    "This is Maple called from Oak"
+
+headingForBirch =
+    "This is Maple called from Birch"
